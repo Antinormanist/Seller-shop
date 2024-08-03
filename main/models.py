e92d158837990193a274ba0fcd379868f833d714
Antinormanist <katya.goryacheva.2009@gmail.com>
2024-08-04 08:33:26 +1000

    Create products model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from user.models import User
+
 # Create your models here.
+class Products(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.SmallIntegerField()
+    description = models.TextField(max_length=999)
+    seller = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    class Meta:
+        db_table = 'products'
