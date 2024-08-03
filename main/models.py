c1d7f341852fa00f8ba2cb05e97267566e80440b
Antinormanist <katya.goryacheva.2009@gmail.com>
2024-08-04 08:38:36 +1000

    create category model

--- main/models.py
+++ main/models.py
@@ -8,9 +8,14 @@ class Products(models.Model):
     price = models.SmallIntegerField()
     description = models.TextField(max_length=999)
     image = models.ImageField(upload_to='product_images')
+    category = models.ForeignKey(to='Categories', on_delete=models.SET_NULL)
     seller = models.ForeignKey(to=User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     
     
     class Meta:
         db_table = 'products'
+        
+        
+class Categories(models.Model):
+    name = models.CharField(max_length=20)
