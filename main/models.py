50984a9f7e39d75b500bd2425308d82e22e8403c
Antinormanist <katya.goryacheva.2009@gmail.com>
2024-08-04 08:34:38 +1000

    forget to add imagefiled

--- main/models.py
+++ main/models.py
@@ -7,6 +7,7 @@ class Products(models.Model):
     name = models.CharField(max_length=20)
     price = models.SmallIntegerField()
     description = models.TextField(max_length=999)
+    image = models.ImageField(upload_to='product_images')
     seller = models.ForeignKey(to=User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     
